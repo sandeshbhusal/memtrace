a56c27c888c1c7ffca8155fb531d08950d9df54d
sandeshbhusal <mail.sandeshbhusal@gmail.com>
2023-03-07 16:09:37 +0545

    feat: added support for tracing PID, binary upcoming

--- src/main.rs
+++ src/main.rs
@@ -1,112 +1,95 @@
-// use std::{error::Error, os::unix::process::CommandExt, process::Command};
+use clap::{Arg, ArgGroup, Command};
+use nix::{
+    libc::exit,
+    sys::{
+        ptrace,
+        ptrace::attach,
+        wait::{waitpid, WaitStatus},
+    },
+    unistd::Pid,
+};
 
-use clap::parser;
-// use nix::{
-//     libc::getpid,
-//     sys::{
-//         ptrace::{self, traceme},
-//         wait::waitpid,
-//     },
-//     unistd::{fork, ForkResult},
-// };
-
-struct PidArgument {}
-
-#[derive(Debug, clap::Parser)]
-struct BinArguments {
-    binary: String,
-}
-
-struct Arguments {}
-
+#[cfg(target_os = "linux")]
 fn main() {
     // Initialize logging.
     env_logger::init();
 
     // Check for binary arguments.
+    let args = Command::new("memtrace").args(&[
+        Arg::new("pid").short('p').long("pid").help("The pid to trace into."),
+        Arg::new("bin").help("The binary to run and trace (will be executed as a child. If it escapes to init, pls kill")
+    ]).group(ArgGroup::new("exclusive_p_b").args(["pid", "bin"]).required(true)).get_matches();
+
+    println!(
+        r#"
+.___  ___.  _______ .___  ___. .___________..______          ___       ______  _______ 
+|   \/   | |   ____||   \/   | |           ||   _  \        /   \     /      ||   ____|
+|  \  /  | |  |__   |  \  /  | `---|  |----`|  |_)  |      /  ^  \   |  ,----'|  |__   
+|  |\/|  | |   __|  |  |\/|  |     |  |     |      /      /  /_\  \  |  |     |   __|  
+|  |  |  | |  |____ |  |  |  |     |  |     |  |\  \----./  _____  \ |  `----.|  |____ 
+|__|  |__| |_______||__|  |__|     |__|     | _| `._____/__/     \__\ \______||_______|
+    "#
+    );
+    if let Some(pid) = args.get_one::<String>("pid") {
+        // Pid based workflow.
+        let pid = pid
+            .parse::<i32>()
+            .map_err(|_| {
+                log::error!(
+                    "{} could not be parsed as a valid PID (only numbers supported). Exiting...",
+                    pid
+                );
+                unsafe { exit(0) }
+            })
+            .unwrap(); // Owned pid.
+
+        let pid = Pid::from_raw(pid);
+
+        // Attach ptrace to the pid.
+        match attach(pid) {
+            Ok(_) => {
+                log::info!("Attached to pid {}", pid);
+            }
+            Err(e) => log::error!("Could not attach to pid {} due to an error: {}", pid, e),
+        }
+
+        // Wait for PID and if successful, run a syscall trace on it.
+        match waitpid(pid, None) {
+            Ok(status) => match status {
+                WaitStatus::Exited(_, _) => {
+                    log::error!("The PID {} has already exited.", pid);
+                }
+
+                _ => {
+                    // Run a syscall trace on it.
+                    ptrace::syscall(pid, None).unwrap();
+                    _ = waitpid(pid, None);
+                    let mut entry_syscall = false;
+                    // While the process does not exit, run a ptrace on it.
+                    loop {
+                        let regs = ptrace::getregs(pid).unwrap();
+                        if entry_syscall {
+                            log::debug!("Got syscall {} at rip {:x}", regs.orig_rax, regs.rip)
+                        }
+
+                        // Resubmit for tracing.
+                        ptrace::syscall(pid, None).unwrap();
+                        entry_syscall = !entry_syscall;
+                        _ = waitpid(pid, None);
+                    }
+                }
+            },
+            Err(e) => {
+                log::error!("Cannot proceed with trace due to an error: {}", e);
+            }
+        }
+    } else {
+        // Binary based workflow.
+    }
 }
 
-// #[cfg(target_os = "linux")]
-// fn main() -> Result<(), Box<dyn Error>> {
-//     // Trace an external program.
-
-//     use std::{env, fs::File, io::Read};
-
-//     use nix::unistd::Pid;
-//     let args = env::args().collect::<Vec<String>>();
-//     if args.len() == 1 {
-//         // continue in self-contained mode.
-//         return fork_and_run();
-//     } else {
-//         // take the first number as pid.
-//         let pid = args.get(1);
-//         if pid.is_none() {
-//             panic!("Wut?");
-//         }
-
-//         let pid = pid.unwrap();
-//         let pid = Pid::from_raw(
-//             pid.parse::<i32>()
-//                 .expect("That does not look like a PID to me."),
-//         );
-//         // Try to read proc filesystem to find all PID sockets.
-//         let filepath = format!("/proc/{}/fd", pid);
-//         let mut procfile = File::open(filepath).unwrap();
-//         let mut buf = String::new();
-//         procfile.read_to_string(&mut buf).unwrap();
-//         for line in buf.lines() {
-//             // Parse line.
-//         }
-//     }
-
-//     Ok(())
-// }
-// fn fork_and_run() -> Result<(), Box<dyn Error>> {
-//     let mut calls_and_locs = std::collections::HashMap::new();
-//     match unsafe { fork() } {
-//         Ok(ForkResult::Parent { child: pid }) => {
-//             let self_pid = unsafe { getpid() };
-//             println!(
-//                 "Child created with PID: {} for parent: {}",
-//                 i32::from(pid),
-//                 self_pid
-//             );
-
-//             let child_pid = pid;
-
-//             waitpid(child_pid, None)?;
-
-//             let mut is_syscall_entry = 0;
-//             loop {
-//                 ptrace::syscall(child_pid, None)?;
-//                 _ = waitpid(child_pid, None)?;
-//                 let regs = nix::sys::ptrace::getregs(pid).unwrap().orig_rax;
-//                 let ripr = nix::sys::ptrace::getregs(pid).unwrap().rip;
-//                 is_syscall_entry += 1;
-//                 if is_syscall_entry % 2 == 0 {
-//                     calls_and_locs.insert(regs, ripr);
-//                     // Also print the current stack trace for the process.
-//                     // let mut stackfile = File::open(format!("/proc/{}/stack", child_pid)).unwrap();
-//                     // let mut buf = String::new();
-//                     // stackfile.read_to_string(&mut buf).unwrap();
-//                     // println!("Stack trace: \n{}", buf);
-//                 }
-//             }
-//         }
-
-//         Ok(ForkResult::Child) => {
-//             // Set current process to be traceable.
-//             let mut command = Command::new("cat");
-//             command.arg("/tmp/outz");
-//             unsafe { command.pre_exec(|| traceme().map_err(|e| e.into())) };
-//             command.exec();
-//             Ok(())
-//         }
-
-//         Err(_) => panic!("Could nnot fork."),
-//     }
-// }
-// #[cfg(target_os = "windows")]
-// fn main() {
-//     println!("Sorry, I do not support windows :(")
-// }
+#[cfg(target_os = "windows")]
+fn main() {
+    env_logger::init();
+    log::error!("Sorry, memtrace does not support windows :(");
+}
